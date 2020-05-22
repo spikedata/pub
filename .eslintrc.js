5845cc44a8eb430f583475f2b46b4074e23b23ed
ilan <ilan@spikedata.co.za>
2020-05-22 10:01:16 +0200

    fix eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,7 @@ module.exports = {
     "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/ban-ts-ignore": "off",
     "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/no-empty-function": "off",
   },
   globals: {
     log: "readonly",
