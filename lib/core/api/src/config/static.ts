bebc25cae8936e705e52c49760df39893ae286bd
ilan <ilan@spikedata.co.za>
2020-10-23 17:07:34 +0200

    changeServer(serviceMap)

--- lib/core/api/src/config/static.ts
+++ lib/core/api/src/config/static.ts
@@ -1,9 +1,9 @@
 import FN from "../function";
 
-const _server = "https://api-v6.spikedata.co.za";
+const _server = "https://api.spikedata.co.za";
 
 export const url = {
-  // web
+  // web-session
   accounts: _server + FN["accounts"].url,
   estatement: _server + FN["estatement"].url,
   login: _server + FN["login"].url,
@@ -18,18 +18,19 @@ export const url = {
   csv: _server + FN["csv"].url,
 };
 
-export function changeServer(server) {
-  // web
-  url.accounts = server + FN["accounts"].url;
-  url.estatement = server + FN["estatement"].url;
-  url.login = server + FN["login"].url;
-  url["login-interim-input"] = server + FN["login-interim-input"].url;
-  url["login-interim-wait"] = server + FN["login-interim-wait"].url;
-  url.statements = server + FN["statements"].url;
-  url.transactions = server + FN["transactions"].url;
-  url.close = server + FN["close"].url;
+// this function is forr internal use
+export function changeServer({ webSession, lambdaPdf, lambdaCsv }) {
+  // web-session
+  url.accounts = webSession + FN["accounts"].url;
+  url.estatement = webSession + FN["estatement"].url;
+  url.login = webSession + FN["login"].url;
+  url["login-interim-input"] = webSession + FN["login-interim-input"].url;
+  url["login-interim-wait"] = webSession + FN["login-interim-wait"].url;
+  url.statements = webSession + FN["statements"].url;
+  url.transactions = webSession + FN["transactions"].url;
+  url.close = webSession + FN["close"].url;
 
   // pdf
-  url.pdf = server + FN["pdf"].url;
-  url.csv = server + FN["csv"].url;
+  url.pdf = lambdaPdf + FN["pdf"].url;
+  url.csv = lambdaCsv + FN["csv"].url;
 }
