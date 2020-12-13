217b09ed22798a8e772339f10e9c63890556afac
ilan <ilan@spikedata.co.za>
2020-12-13 18:04:19 +0200

    link: conf

--- samples/web-link-cli/src/config.js
+++ samples/web-link-cli/src/config.js
@@ -1,6 +1,6 @@
 // const linkUrl = "https://link.spikedata.co.za";
-const linkUrl = "http://localhost:3010"; // debug sx + local stack (not front-end)
-// const linkUrl = "http://localhost:8080"; // use vue dev server for debugging front end
+// const linkUrl = "http://localhost:3010"; // debug sx + local stack (not front-end)
+const linkUrl = "http://localhost:8080"; // use vue dev server for debugging front end
 const localPort = 3000;
 
 module.exports = {
