2c4de19d0ba22610b534ddd05c00254493025804
ilan <ilan@spikedata.co.za>
2020-12-14 10:58:18 +0200

    isUserError: catch ShapeNotFoundError

--- lib/core/api/src/helpers.ts
+++ lib/core/api/src/helpers.ts
@@ -5,6 +5,11 @@ export function isUserError(response) {
   const isError = Enums.TYPES.ERROR === response.type;
   if (!isError) return false;
   // check shape
-  const shape = Shapes.getShape(response.code);
-  return shape.blame == Enums.BLAME.USER;
+  try {
+    const shape = Shapes.getShape(response.code);
+    return shape.blame == Enums.BLAME.USER;
+  } catch (e) {
+    // ShapeNotFoundError
+    return false;
+  }
 }
